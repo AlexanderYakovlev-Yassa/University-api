6de0b2a6649bf4e61be20424539fc488f8b48037
Alexander Yakovlev <sashayassa@gmail.com>
2021-02-14 10:52:32 +0300

    update SecurityConfig

--- src/main/java/ua/foxminded/yakovlev/university/init/SecurityConfig.java
+++ src/main/java/ua/foxminded/yakovlev/university/init/SecurityConfig.java
@@ -1,15 +1,19 @@
 package ua.foxminded.yakovlev.university.init;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.AllArgsConstructor;
+import ua.foxminded.yakovlev.university.filters.JwtRequestFilter;
 import ua.foxminded.yakovlev.university.service.impl.UserService;
 
 @EnableWebSecurity
@@ -18,18 +22,27 @@ import ua.foxminded.yakovlev.university.service.impl.UserService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private UserService userServise;
+	private JwtRequestFilter jwtRequestFilter;
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {		
+		auth.userDetailsService(userServise)
+		.passwordEncoder(passwordEncoder());
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http
 		.csrf().disable()
 		.authorizeRequests()
+		.antMatchers("/api/authenticate").permitAll()
 		.antMatchers("/").permitAll()
+		.anyRequest().authenticated()
 		.and()
-		.formLogin()
-		.and()
-		.logout().logoutSuccessUrl("/index");
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean
@@ -37,13 +50,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Override
 	@Bean
-	public DaoAuthenticationProvider daoAuthenticationProvider() {
-		
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
-		authenticationProvider.setUserDetailsService(userServise);
-		
-		return authenticationProvider;
+	public AuthenticationManager authenticationManagerBean() throws Exception {		
+		return super.authenticationManagerBean();
 	}
 }
