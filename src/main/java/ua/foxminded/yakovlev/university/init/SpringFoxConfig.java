8db50459a12b1ec59eab49d077f9ddf44e398b2e
Alexander Yakovlev <sashayassa@gmail.com>
2021-03-11 09:54:32 +0300

    fix swagger doc

--- src/main/java/ua/foxminded/yakovlev/university/init/SpringFoxConfig.java
+++ src/main/java/ua/foxminded/yakovlev/university/init/SpringFoxConfig.java
@@ -1,6 +1,8 @@
 package ua.foxminded.yakovlev.university.init;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,12 @@ import org.springframework.core.env.Environment;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -35,11 +41,28 @@ public class SpringFoxConfig {
     public Docket api() {
     	
         return new Docket(DocumentationType.SWAGGER_2)
+        		.securityContexts(Arrays.asList(securityContext()))
+        		.securitySchemes(Arrays.asList(apiKey()))
           .select()
           .apis(RequestHandlerSelectors.basePackage(env.getProperty(BASE_PACKAGE)))
           .paths(PathSelectors.any())
           .build()
           .apiInfo(apiDetail());
+    }
+    
+    private SecurityContext securityContext() { 
+        return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
+    }
+    
+    private List<SecurityReference> defaultAuth() { 
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
+        authorizationScopes[0] = authorizationScope; 
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
+    }
+    
+    private ApiKey apiKey() { 
+        return new ApiKey("JWT", "Authorization", "header"); 
     }
     
     private ApiInfo apiDetail() {
